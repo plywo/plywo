d67094add4aacf2d56a8230d71109fe77a812343
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-05 02:51:41 +0300

    Reap overdue finalization leases

--- app/jobs/github_pull_request_execution_lease_reaper_job.rb
+++ app/jobs/github_pull_request_execution_lease_reaper_job.rb
@@ -3,7 +3,7 @@ class GithubPullRequestExecutionLeaseReaperJob < ApplicationJob
 
   def perform(now = Time.current)
     PlywoExecution
-      .where(source: "github_pull_request", status: "running")
+      .where(source: "github_pull_request", status: PlywoExecution::LEASED_STATUSES)
       .where("lease_expires_at <= ?", now)
       .find_each do |execution|
         expiry_job_class.perform_later(execution.id, now)
