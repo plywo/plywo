f8c36df12e618f855b99e202e2df1e786ed8fba8
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-04 15:10:59 +0300

    Route process isolation demo

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   if Rails.env.development? || Rails.env.test?
     post "/__plywo/demo/behavior" => "demo/behavior#create"
+    post "/__plywo/demo/process-proof" => "demo/process_proof#create"
   end
 end
