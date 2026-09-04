8e56965f74010b3e096b97e03b4cb94d64b0b157
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-04 15:20:02 +0300

    Make Solid Queue opt-in for integration tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,7 +3,15 @@ Rails.application.configure do
   config.eager_load = ENV["CI"].present?
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
-  config.active_job.queue_adapter = :test
+
+  if ENV["PLYWO_SOLID_QUEUE"] == "1"
+    config.active_job.queue_adapter = :solid_queue
+    config.solid_queue.connects_to = { database: { writing: :queue } }
+    config.solid_queue.logger = ActiveSupport::Logger.new($stdout)
+  else
+    config.active_job.queue_adapter = :test
+  end
+
   config.action_mailer.delivery_method = :test
   config.action_mailer.perform_deliveries = true
   config.active_support.deprecation = :stderr
