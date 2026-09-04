34cd54c9ea2fdfbf183fe7fa74114ffd9c23a41c
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-04 15:26:18 +0300

    Run correlated executions through Solid Queue
    * Add Solid Queue transport
    
    * Configure dedicated Solid Queue database
    
    * Make Solid Queue opt-in for integration tests
    
    * Configure Solid Queue for production
    
    * Configure Solid Queue workers
    
    * Add Solid Queue recurring config
    
    * Add Solid Queue worker executable
    
    * Add Solid Queue database schema
    
    * Add real Solid Queue transport proof
    
    * Dogfood real Solid Queue transport in CI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,4 +5,6 @@ Rails.application.configure do
   config.force_ssl = ENV["RAILS_FORCE_SSL"] == "1"
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
   config.log_tags = [ :request_id ]
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 end
