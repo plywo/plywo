859259aa0206d63c42afec9c47aec6d9e940feea
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-05 00:32:04 +0300

    Resolve executor service worker adapter

---
+++ lib/plywo/executor/service_resolver.rb
@@ -0,0 +1,20 @@
+module Plywo
+  module Executor
+    class ServiceResolver
+      Error = Class.new(StandardError)
+
+      def self.from_env(root: ::Rails.root, env: ENV)
+        mode = env.fetch("PLYWO_EXECUTOR_SERVICE_ADAPTER", "local")
+
+        case mode
+        when "local"
+          LocalAdapter.new(root:)
+        when "disabled"
+          raise Error, "Plywo executor service adapter is disabled"
+        else
+          raise Error, "Unsupported PLYWO_EXECUTOR_SERVICE_ADAPTER=#{mode.inspect}"
+        end
+      end
+    end
+  end
+end
