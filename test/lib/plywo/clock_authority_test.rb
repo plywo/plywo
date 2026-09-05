88e1d49675f912722d465248392bee9e43a09091
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-05 03:46:40 +0300

    Test database clock without mock dependency

--- test/lib/plywo/clock_authority_test.rb
+++ test/lib/plywo/clock_authority_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class PlywoClockAuthorityTest < ActiveSupport::TestCase
   test "reads wall time from PostgreSQL clock_timestamp" do
-    connection = Minitest::Mock.new
-    connection.expect(
-      :select_value,
-      "2026-09-05 00:40:00+00",
-      [ Plywo::ClockAuthority::DATABASE_NOW_SQL, "Plywo database clock" ]
-    )
+    calls = []
+    connection = Object.new
+    connection.define_singleton_method(:select_value) do |sql, name|
+      calls << [ sql, name ]
+      "2026-09-05 00:40:00+00"
+    end
 
     assert_equal Time.zone.parse("2026-09-05 00:40:00 UTC"), Plywo::ClockAuthority.database_now(connection:)
-    connection.verify
+    assert_equal [ [ Plywo::ClockAuthority::DATABASE_NOW_SQL, "Plywo database clock" ] ], calls
   end
 
   test "monotonic clock never exposes wall-clock time" do
