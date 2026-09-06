66be69f5b3b1140810885eaeaaa836ba1306016c
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-07 02:18:31 +0300

    Trace customer SQLite db preparation

--- lib/plywo/subject/rails_sqlite_environment.rb
+++ lib/plywo/subject/rails_sqlite_environment.rb
@@ -41,7 +41,7 @@ module Plywo
         env = env_for(root:, execution:, role:)
         @command_runner.call(
           env:,
-          command: [ RbConfig.ruby, root.join("bin", "rails").to_s, "db:prepare" ],
+          command: [ RbConfig.ruby, root.join("bin", "rails").to_s, "db:prepare", "--trace" ],
           chdir: root.to_s
         )
         env
