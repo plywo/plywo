1b2f0444e18fb0f303c9c0f24f0e904ee537a676
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-06 16:52:44 +0300

    Add subject environment capability contract

--- lib/plywo/subject/environment.rb
+++ lib/plywo/subject/environment.rb
@@ -1,6 +1,24 @@
 module Plywo
   module Subject
     class Environment
+      EMPTY_CAPABILITIES = [].freeze
+
+      def capabilities
+        EMPTY_CAPABILITIES
+      end
+
+      def capability?(name)
+        capabilities.include?(name.to_s)
+      end
+
+      def capabilities_for(namespace)
+        prefix = "#{namespace}."
+
+        capabilities.filter_map do |capability|
+          capability.delete_prefix(prefix) if capability.start_with?(prefix)
+        end
+      end
+
       def prepare(root:, execution:, role:)
         raise NotImplementedError
       end
