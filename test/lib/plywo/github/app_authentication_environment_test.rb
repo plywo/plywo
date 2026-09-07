a4044e41bf4da12509eb9a64adb1d51d576fd2ac
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-07 12:34:08 +0300

    Test GitHub API endpoint without minitest mocks

--- test/lib/plywo/github/app_authentication_environment_test.rb
+++ test/lib/plywo/github/app_authentication_environment_test.rb
@@ -10,22 +10,12 @@ class PlywoGithubAppAuthenticationEnvironmentTest < ActiveSupport::TestCase
     ENV["PLYWO_GITHUB_PRIVATE_KEY_PATH"] = "tmp/lab-app.pem"
     ENV["PLYWO_GITHUB_APP_ID"] = "12345"
     ENV["GITHUB_API_URL"] = "http://github-emulator:4001"
-    captured = nil
-    authentication = Object.new
 
-    factory = lambda do |**attributes|
-      captured = attributes
-      authentication
-    end
+    authentication = Plywo::Github::AppAuthentication.from_env(root: "/srv/plywo")
 
-    result = Plywo::Github::AppAuthentication.stub(:new, factory) do
-      Plywo::Github::AppAuthentication.from_env(root: "/srv/plywo")
-    end
-
-    assert_same authentication, result
-    assert_equal "12345", captured.fetch(:app_id)
-    assert_equal "/srv/plywo/tmp/lab-app.pem", captured.fetch(:private_key_path)
-    assert_equal "http://github-emulator:4001", captured.fetch(:api_url)
+    assert_equal "12345", authentication.instance_variable_get(:@app_id)
+    assert_equal "/srv/plywo/tmp/lab-app.pem", authentication.instance_variable_get(:@private_key_path)
+    assert_equal "http://github-emulator:4001", authentication.instance_variable_get(:@api_url)
   ensure
     previous&.each do |name, value|
       value.nil? ? ENV.delete(name) : ENV[name] = value
