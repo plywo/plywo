3eaf150c6b7a46e77420a7fc640e33bf893ac42f
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-08 16:35:22 +0300

    Ship Yarn Berry as a declared production executor capability (#112)
    * Ship Yarn Berry in production executor image
    
    * Prove Yarn Berry executor capability
    
    * Isolate Yarn Berry installation from Node image shims
    
    * Use Yarn Berry skip-build mode in proof setup

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,16 @@ ARG RUBY_VERSION=3.4.10
 ARG NODE_VERSION=24.20.0
 ARG NPM_VERSION=11.19.0
 ARG PNPM_VERSION=12.3.4
+ARG YARN_VERSION=4.18.0
 
 FROM node:${NODE_VERSION}-slim AS node_runtime
 ARG PNPM_VERSION
+ARG YARN_VERSION
 RUN npm install --global "pnpm@${PNPM_VERSION}" && \
-    test "$(pnpm --version)" = "${PNPM_VERSION}"
+    npm install --prefix /opt/yarn --omit=dev --no-package-lock "@yarnpkg/cli-dist@${YARN_VERSION}" && \
+    YARN_BIN="$(node -p "require('/opt/yarn/node_modules/@yarnpkg/cli-dist/package.json').bin.yarn")" && \
+    test "$(pnpm --version)" = "${PNPM_VERSION}" && \
+    test "$(node "/opt/yarn/node_modules/@yarnpkg/cli-dist/${YARN_BIN}" --version)" = "${YARN_VERSION}"
 
 FROM ruby:${RUBY_VERSION}-slim
 
@@ -16,13 +21,14 @@ ARG RUBY_VERSION
 ARG NODE_VERSION
 ARG NPM_VERSION
 ARG PNPM_VERSION
+ARG YARN_VERSION
 
 WORKDIR /app
 
 ENV BUNDLE_DEPLOYMENT=1 \
     BUNDLE_PATH=/usr/local/bundle \
     BUNDLE_WITHOUT=development \
-    PLYWO_EXECUTOR_CAPABILITIES_JSON="{\"runtimes\":{\"ruby\":\"${RUBY_VERSION}\",\"node\":\"${NODE_VERSION}\"},\"package_managers\":{\"npm\":\"${NPM_VERSION}\",\"pnpm\":\"${PNPM_VERSION}\"}}"
+    PLYWO_EXECUTOR_CAPABILITIES_JSON="{\"runtimes\":{\"ruby\":\"${RUBY_VERSION}\",\"node\":\"${NODE_VERSION}\"},\"package_managers\":{\"npm\":\"${NPM_VERSION}\",\"pnpm\":\"${PNPM_VERSION}\",\"yarn\":\"${YARN_VERSION}\"}}"
 
 RUN apt-get update -qq && \
     apt-get install --no-install-recommends -y \
@@ -36,23 +42,28 @@ RUN apt-get update -qq && \
 COPY --from=node_runtime /usr/local/bin/node /usr/local/bin/node
 COPY --from=node_runtime /usr/local/lib/node_modules/npm /usr/local/lib/node_modules/npm
 COPY --from=node_runtime /usr/local/lib/node_modules/pnpm /usr/local/lib/node_modules/pnpm
+COPY --from=node_runtime /opt/yarn /opt/yarn
 
 RUN ln -s ../lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm && \
     ln -s ../lib/node_modules/npm/bin/npx-cli.js /usr/local/bin/npx && \
     PNPM_BIN="$(node -p "require('/usr/local/lib/node_modules/pnpm/package.json').bin.pnpm")" && \
     PNPX_BIN="$(node -p "require('/usr/local/lib/node_modules/pnpm/package.json').bin.pnpx")" && \
+    YARN_BIN="$(node -p "require('/opt/yarn/node_modules/@yarnpkg/cli-dist/package.json').bin.yarn")" && \
     ln -s "../lib/node_modules/pnpm/${PNPM_BIN}" /usr/local/bin/pnpm && \
     ln -s "../lib/node_modules/pnpm/${PNPX_BIN}" /usr/local/bin/pnpx && \
+    ln -s "/opt/yarn/node_modules/@yarnpkg/cli-dist/${YARN_BIN}" /usr/local/bin/yarn && \
     test "$(node --version)" = "v${NODE_VERSION}" && \
     test "$(npm --version)" = "${NPM_VERSION}" && \
-    test "$(pnpm --version)" = "${PNPM_VERSION}"
+    test "$(pnpm --version)" = "${PNPM_VERSION}" && \
+    test "$(yarn --version)" = "${YARN_VERSION}"
 
 COPY Gemfile Gemfile.lock ./
 RUN bundle install
 
 COPY . .
 RUN bundle exec ruby script/prove_node_npm_executor_capability.rb && \
-    bundle exec ruby script/prove_pnpm_executor_capability.rb
+    bundle exec ruby script/prove_pnpm_executor_capability.rb && \
+    bundle exec ruby script/prove_yarn_berry_executor_capability.rb
 
 ENV RAILS_ENV=production \
     RAILS_LOG_TO_STDOUT=1
---
+++ script/prove_yarn_berry_executor_capability.rb
@@ -0,0 +1,163 @@
+#!/usr/bin/env ruby
+
+require "active_support/core_ext/object/blank"
+require "digest"
+require "fileutils"
+require "json"
+require "pathname"
+require "tmpdir"
+
+TOOL_ROOT = Pathname(__dir__).join("..").expand_path.freeze
+
+require TOOL_ROOT.join("lib", "plywo", "subject", "runtime_capabilities").to_s
+require TOOL_ROOT.join("lib", "plywo", "subject", "setup_plan").to_s
+require TOOL_ROOT.join("lib", "plywo", "subject", "bootstrap_executor").to_s
+require TOOL_ROOT.join("lib", "plywo", "subject", "javascript_package_manager_detector").to_s
+require TOOL_ROOT.join("lib", "plywo", "subject", "javascript_dependencies_bootstrap").to_s
+require TOOL_ROOT.join("lib", "plywo", "github", "local_pull_request_runner").to_s
+
+module YarnBerryExecutorCapabilityProof
+  EXPECTED_NODE_VERSION = "24.20.0"
+  EXPECTED_YARN_VERSION = "4.18.0"
+
+  module_function
+
+  def call
+    capabilities = Plywo::Subject::RuntimeCapabilities.from_env
+    assert_capabilities!(capabilities)
+
+    command_runner = Plywo::Github::LocalPullRequestRunner::CommandRunner.new
+    node_runtime = run_version!(command_runner, %w[node --version])
+    yarn_runtime = run_version!(command_runner, %w[yarn --version])
+
+    unless node_runtime == "v#{EXPECTED_NODE_VERSION}"
+      raise "Declared Node capability does not match runtime: #{node_runtime.inspect}"
+    end
+    unless yarn_runtime == EXPECTED_YARN_VERSION
+      raise "Declared Yarn capability does not match runtime: #{yarn_runtime.inspect}"
+    end
+
+    Dir.mktmpdir("plywo-yarn-berry-capability-") do |directory|
+      root = Pathname(directory)
+      write_subject(root)
+      generate_lockfile!(command_runner, root)
+      remove_generated_install_state!(root)
+
+      detection = Plywo::Subject::JavascriptPackageManagerDetector.new.call(root:)
+      unless detection&.manager == "yarn" && detection.lockfile == "yarn.lock"
+        raise "Expected Yarn package-manager detection from yarn.lock"
+      end
+      unless detection.yarn_generation == "berry"
+        raise "Expected Yarn Berry generation evidence"
+      end
+      unless detection.package_manager_version == EXPECTED_YARN_VERSION
+        raise "Expected exact Yarn packageManager version evidence"
+      end
+
+      manifest = root.join("package.json")
+      lockfile = root.join("yarn.lock")
+      manifest_digest = Digest::SHA256.file(manifest).hexdigest
+      lockfile_digest = Digest::SHA256.file(lockfile).hexdigest
+
+      javascript_bootstrap = Plywo::Subject::JavascriptDependenciesBootstrap.new(command_runner:)
+      bootstrap_executor = Plywo::Subject::BootstrapExecutor.new(
+        ruby_bundle_bootstrap: nil,
+        javascript_dependencies_bootstrap: javascript_bootstrap,
+        runtime_capabilities: capabilities
+      )
+      setup_plan = Plywo::Subject::SetupPlan.new(
+        framework: "javascript",
+        steps: [ detection.bootstrap_step ]
+      )
+      bootstrap_executor.call(root:, setup_plan:)
+
+      pnp_runtime = root.join(".pnp.cjs")
+      raise "Yarn immutable install did not recreate the PnP runtime" unless pnp_runtime.file?
+      assert_digest!(manifest, manifest_digest, label: "package.json")
+      assert_digest!(lockfile, lockfile_digest, label: "yarn.lock")
+
+      dependency_runtime = command_runner.call(
+        env: {},
+        command: [
+          "yarn",
+          "node",
+          "-e",
+          "process.stdout.write(process.version + ':' + String(require('is-number')(42)))"
+        ],
+        chdir: root.to_s
+      ).strip
+      expected_dependency_runtime = "v#{EXPECTED_NODE_VERSION}:true"
+      unless dependency_runtime == expected_dependency_runtime
+        raise "Yarn-installed dependency used unexpected runtime: #{dependency_runtime.inspect}"
+      end
+
+      puts "Yarn Berry executor capability proof"
+      puts "executor_node_capability=#{capabilities.runtime_version("node")}"
+      puts "executor_yarn_capability=#{capabilities.package_manager_version("yarn")}"
+      puts "node_runtime=#{node_runtime}"
+      puts "yarn_runtime=#{yarn_runtime}"
+      puts "javascript_package_manager=yarn"
+      puts "yarn_generation=berry"
+      puts "package_manager_version_contract=matched"
+      puts "dependency_bootstrap=yarn_install_immutable"
+      puts "customer_dependency_runtime=#{dependency_runtime}"
+      puts "committed_dependency_contract_unchanged=true"
+    end
+  end
+
+  def assert_capabilities!(capabilities)
+    unless capabilities.runtime_version("ruby") == RUBY_VERSION
+      raise "Executor Ruby capability does not match runtime Ruby #{RUBY_VERSION}"
+    end
+    unless capabilities.runtime_version("node") == EXPECTED_NODE_VERSION
+      raise "Expected declared Node capability #{EXPECTED_NODE_VERSION}"
+    end
+    unless capabilities.package_manager_version("yarn") == EXPECTED_YARN_VERSION
+      raise "Expected declared Yarn capability #{EXPECTED_YARN_VERSION}"
+    end
+  end
+
+  def run_version!(command_runner, command)
+    command_runner.call(env: {}, command:, chdir: TOOL_ROOT.to_s).strip
+  end
+
+  def generate_lockfile!(command_runner, root)
+    command_runner.call(
+      env: {},
+      command: %w[yarn install --mode=skip-build],
+      chdir: root.to_s
+    )
+
+    lockfile = root.join("yarn.lock")
+    raise "Yarn did not generate yarn.lock for the proof subject" unless lockfile.file?
+  end
+
+  def remove_generated_install_state!(root)
+    FileUtils.rm_f(root.join(".pnp.cjs"))
+    FileUtils.rm_f(root.join(".pnp.loader.mjs"))
+    FileUtils.rm_rf(root.join(".yarn"))
+  end
+
+  def write_subject(root)
+    package = {
+      "name" => "plywo-yarn-berry-capability-proof",
+      "version" => "1.0.0",
+      "private" => true,
+      "packageManager" => "yarn@#{EXPECTED_YARN_VERSION}",
+      "dependencies" => {
+        "is-number" => "7.0.0"
+      }
+    }
+
+    root.join("package.json").write("#{JSON.pretty_generate(package)}\n")
+  end
+
+  def assert_digest!(path, expected_digest, label:)
+    actual_digest = Digest::SHA256.file(path).hexdigest
+    return if actual_digest == expected_digest
+
+    raise "Yarn bootstrap mutated committed #{label}"
+  end
+end
+
+YarnBerryExecutorCapabilityProof.call
